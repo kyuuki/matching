6c58c0b3ea93525ffe7657a5a371e56764f636e9
kyuuki <fdear.kobayashi@nifty.com>
2016-07-20 07:14:02 +0000

    Add login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :admin_users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
