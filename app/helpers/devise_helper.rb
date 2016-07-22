bd1c0b57f271bf85c807834d6d65315f6d17c21b
kyuuki <fdear.kobayashi@nifty.com>
2016-07-22 08:44:10 +0000

    Refine user registration and login screens

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,18 @@
+# https://github.com/plataformatec/devise/wiki/How-To:-Integrate-I18n-Flash-Messages-with-Devise-and-Bootstrap
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-error alert-danger"> <button type="button"
+    class="close" data-dismiss="alert">&times;</button>
+      <ul>
+        #{messages}
+      </ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
