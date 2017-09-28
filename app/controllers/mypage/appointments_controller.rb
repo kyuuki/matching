15b628c0f5b7f72e94ca9c788ed068fb7c3ddae4
kyuuki <fdear.kobayashi@nifty.com>
2017-09-28 03:00:43 +0000

    Modify view of appointments

--- app/controllers/mypage/appointments_controller.rb
+++ app/controllers/mypage/appointments_controller.rb
@@ -2,7 +2,7 @@ class Mypage::AppointmentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @appointments = Appointment.where(user: current_user)
+    @appointments = Appointment.where(user: current_user).order({ datetime: :desc })
 
     render layout: "mypage"
   end
